7fed94015e5a098ecda408c0054a1191a7cbfc9c
kiyosumi-png <shoudaoqingcheng3@gmail.com>
2021-04-24 18:36:40 +0900

    Reactで書き換える

--- introduce/src/App.js
+++ introduce/src/App.js
@@ -1,25 +1,125 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Link} from 'react-router-dom';
+import { createStore } from 'redux';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// BrowserRouterでフロントの画面遷移
+// Routeでパスを指定
+const App = () => (
+  <BrowserRouter>
+      <div>
+        <Route exact path="/associative" component={Split} />
+        <Route path="/associative/supporter" component={Supporter} />
+        <Route path="/associative/solver" component={Solver} />
+        <Route path="/associative/answer" component={Answer} />
+      </div>
+    </BrowserRouter>
+)
+
+
+const counter  = (state=1, action)  => {
+  switch (action.type) {
+    case 'ADD':
+      return state  + 1
+      default:
+      return state
+    }
 }
 
+let store = createStore(counter)
+
+const Split = () => {
+  store.dispatch({  type:  'ADD'  })
+
+  return(
+    <div>
+      <p><Link to="/associative/supporter">ヒントを出す人はこちら</Link></p>
+      <p><Link to="/associative/solver">答えを書く人はこちら</Link></p>
+    </div>
+  )
+}
+
+const Supporter = () => {
+  let count = store.getState().toString()
+
+  return(
+    <div>
+      <h1>お題</h1>
+      <p>{count}問目のみんなのヒント</p>
+    </div>
+  )
+}
+
+const Solver = () => {
+  return(
+    <div>
+      <h1>ヒント</h1>
+    </div>
+  )
+}
+
+
 export default App;
+// // React.componentを拡張してClockを作るJSXで書く
+// class Clock extends React.Component {
+//   // Clockクラスが作られたときに呼ばれる特別なメソッド
+//   // superはreact.componentを指している今受け取ったpropsを渡す
+//   constructor(props) {
+//     super(props);
+//     this.now = new Date();
+
+//     this.state = {
+  //       time: `${this.now.getHours()}:${this.now.getMinutes()}:${this.now.getSeconds()}`
+//     }
+
+//     this.refresh = this.refresh.bind(this);
+//   }
+
+//   // 定義したstateをsetStateで書き換える
+//   refresh() {
+  //     this.now = new Date();
+
+//     this.setState((state) => ({
+//       time: `${this.now.getHours()}:${this.now.getMinutes()}:${this.now.getSeconds()}`
+//     }))
+//   }
+
+//   render() {
+   
+//     return <p onClick={this.refresh}>{this.state.time}</p>
+//   }
+// }
+
+
+// npm run start
+
+// Reduxステートを管理するもの
+// サイトの中で共通で使いたいものがあるときに使う
+// npm でreduxをインストール
+// react-reduxもインストール
+
+// Routing
+// npmでreact-router-domをインストール
+
+
+
+// const Blog = props => {
+//   // matchするidがあればとってくる
+//   const { id } = props.match.params
+
+//   return(
+//     <div>
+//       <p>{id}番目の記事です</p>
+//     </div>
+//   )
+// }
+
+// const Sum = props => {
+//   const { num1, num2 } = props.match.params
+
+//   return (
+//     <div>
+//       <p>{num1} + {num2} = {parseInt(num1) + parseInt(num2)}</p>
+//     </div>
+//   )
+// }
